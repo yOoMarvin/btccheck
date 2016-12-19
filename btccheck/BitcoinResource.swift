3459ff87f5ab958e728d20ae573d66af34ab5bef
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2016-12-19 21:48:42 +0100

    Adding data model

---
+++ btccheck/BitcoinResource.swift
@@ -0,0 +1,24 @@
+//
+//  BitcoinResource.swift
+//  btccheck
+//
+//  Created by Marvin Messenzehl on 19.12.16.
+//  Copyright © 2016 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+struct BitcoinResource {
+    let url: String
+}
+
+
+//Build URL: in this case simpel
+
+//Download Data
+
+    //async
+
+    //completion(...)
+
+//Do something with data
---
+++ btccheck/BitcoinResponse.swift
@@ -0,0 +1,19 @@
+//
+//  BitcoinResponse.swift
+//  btccheck
+//
+//  Created by Marvin Messenzehl on 19.12.16.
+//  Copyright © 2016 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+struct BitcoinResponse {
+    let price: Double
+    let high: Double
+    let low: Double
+    let open: Double
+    let avg: Double
+    let volume: Double
+    let change: Double
+}
