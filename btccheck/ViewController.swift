4a47bf006bde4473f4cae207a2e69a8789322899
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2016-12-19 19:52:14 +0100

    Initial commit from local git

---
+++ btccheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  btccheck
+//
+//  Created by Marvin Messenzehl on 19.12.16.
+//  Copyright © 2016 Marvin Messenzehl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
