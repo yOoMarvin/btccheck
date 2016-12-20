b51527ff0da3cf7bb2ac0fece132c0d91be174ff
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2016-12-20 19:59:15 +0100

    Added history data models

---
+++ btccheck/HistoryResource.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryResource.swift
+//  btccheck
+//
+//  Created by Marvin Messenzehl on 20.12.16.
+//  Copyright © 2016 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+class HistoryResource{
+    //build URL
+    
+    //Download data for bitcoin history
+    
+    
+}
---
+++ btccheck/HistoryResponse.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryResponse.swift
+//  btccheck
+//
+//  Created by Marvin Messenzehl on 20.12.16.
+//  Copyright © 2016 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+struct HistoryResponse {
+    let dates:[String]
+    let prices:[Double]
+}
